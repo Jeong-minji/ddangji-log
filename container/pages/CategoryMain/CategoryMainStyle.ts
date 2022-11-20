7827713ac41358eba675e6f8a9a8639505d67af3
Jeong-minji <alswl4152@naver.com>
2022-11-20 16:05:06 +0900

    Fix: build error

--- container/pages/CategoryMain/CategoryMainStyle.ts
+++
@@ -1,6 +0,0 @@
-import styled from "@emotion/styled";
-
-export const CategoryMainWrapper = styled.div`
-  padding: ${({ theme }) => theme.figure * 20}px
-    ${({ theme }) => theme.figure * 10}px;
-`;
